d1ba8da758adc48d21d1d589a01dd9feb800ea00
ftdkeating <ftdwlei@126.com>
2024-05-08 23:32:12 +0800

    ch01

---
+++ Ex1_Python_Crash_Course/ch01/hello_world.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+@Time ： 2024/5/8 23:28
+@Author ： captain
+@File ：hello_world.py
+"""
+print("Hello Python World!")
---
+++ Ex1_Python_Crash_Course/ch01/web_browse.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+@Time ： 2024/5/7 23:57
+@Author ： captain
+@File ：web_browse.py
+"""
+import webbrowser
+
+webbrowser.open("python.org")
