5201829852a551141ab86984035aaa662746bdad
unityofkaa <unityofkaa@126.com>
2024-05-09 23:49:05 +0800

    ch05

---
+++ Ex1_Python_Crash_Course/ch05/if_test.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+@Time ： 2024/5/9 23:47
+@Author ： captain
+@File ：if_test.py
+"""
+car = "subaru"
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru')
+print("Is car == 'audi'? I predict False.")
+print(car == 'audi')
