f13a7cb6467e0ee6219573824b7d47bcae57e5f2
unityofkaa <unityofkaa@126.com>
2024-08-06 23:39:07 +0800

    exercise

---
+++ Ex2_Learn_Python_The_Hard_Way/ex_01.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Time: 2024/8/6 23:09
+@Author: captain
+@File: ex_01.py
+"""
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print('Yay! Printing.')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
---
+++ Ex2_Learn_Python_The_Hard_Way/ex_02.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Time: 2024/8/6 23:21
+@Author: captain
+@File: ex_02.py
+"""
+# A comment, this is so you can read your program later.
+# Anything after the # is ignored by python.
+print("I could have code like this.")  # and the comment after is ignored
+# You can also use a comment to "disable" or comment out code:
+# print("This won't run.")
+print("This will run.")
